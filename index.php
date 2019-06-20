20a59dc542d2e0453d30874e7e86bed26e5fcc3f
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-06-20 21:59:08 +0200

    new url cinoversity.de

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ header( 'Location: /index.html' );
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
-  ?>
+?>
